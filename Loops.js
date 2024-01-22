eb6bd19123e55642005efe6dd7ffaa8ad0e78812
huzaifa shamim <deensayingsss@gmail.com>
2024-01-22 21:48:47 +0530

    update

---
+++ Loops.js
@@ -0,0 +1,44 @@
+// There are 3  types of loops 
+// for loops
+// for (i=1; i<=10;i++){
+//     console.log(i);
+// }
+// for (i=0; i<100;i++){
+  
+//     console.log("Hello Huzaifa");
+// }
+// let coding_language=["Javascript","python","Rust","Java"];
+// for(i=0;i<coding_language.length;i++){
+//     console.log(coding_language[i]);
+// }
+
+// for (let i = 0; i <= 5; i++) {
+//    console.log(i);
+//    for (let j = 0; j <3; j++) {
+//     console.log("loop") 
+//    }   
+// }
+
+// while loops
+// let i=0;
+// while (i<=5) {
+//     console.log("We found it");
+//     i++;
+// }
+
+
+// do while
+// let i=10;
+// do {
+//     console.log(i);
+//     i++;
+// }while(i<=5);
+
+// break statement / Continue Statement
+// for (i=1;i<=5;i++){
+//     if(i==2){
+//         continue;
+//     }
+   
+//     console.log(i);
+// }
