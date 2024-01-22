825362ba8136c38b6d1f5c8258c187e4380c5e28
huzaifa shamim <deensayingsss@gmail.com>
2024-01-22 11:54:50 +0530

    update

---
+++ DataType.js
@@ -0,0 +1,90 @@
+// There are 8 basic data types in JavaScript.
+
+// Seven primitive data types:
+// number for numbers of any kind: integer or floating-point, integers are limited by ±(253-1).
+// bigint for integer numbers of arbitrary length.
+// string for strings. A string may have zero or more characters, there’s no separate single-character type.
+// boolean for true/false.
+// null for unknown values – a standalone type that has a single value null.
+// undefined for unassigned values – a standalone type that has a single value undefined.
+// symbol for unique identifiers.
+// And one non-primitive data type:
+// object for more complex data structures.
+
+
+// Number DataType
+let x=30;
+console.log(x);
+
+// String DataType
+// we can write string dataType in "",''
+var First ="Hello world";
+console.log(First);
+console.log(typeof(x));
+console.log(typeof(First));
+
+
+// boolean DataType
+const Y= true;
+console.log(Y);
+console.log(typeof(Y));
+ let z= 30<20;
+ console.log(z);
+ console.log(typeof(z));
+
+ //undefined DataType
+ let age
+ console.log(age);
+
+  //null DataType 
+  let value=null
+  console.log(value);
+  console.log(typeof(value));
+
+
+//   Reference DataType
+
+// Object reference 
+  let value1=null;
+  console.log(value1);
+  console.log(typeof(value1));
+
+  let Name={
+    FirstName:"Elon",
+    SecondName:"musk"
+  }
+  console.log(Name);
+  console.log(typeof(Name));
+
+  //Array DataType
+
+  let number=[1,2,3,4,5,6,7,8,9,10];
+  console.log(typeof(number));
+  console.log(number);
+
+  ///Function DataTYpe
+
+  function Huzaifa(){
+    console.log("Assalamua alaikum");
+  }
+  Huzaifa();
+  console.log(typeof Huzaifa);
+  console.log(Huzaifa);
+
+
+  ///DataType
+  X=100;
+  console.log(X);
+  console.log(typeof X);
+
+
+  X="Assalamu alaikum";
+  console.log(X);
+  console.log(typeof X);
+
+
+  X=true;
+  console.log(X);
+  console.log(typeof X);
+  
+  
